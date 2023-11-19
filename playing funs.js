47dd3a6cc50bb32b09514fab87b4691d231f5ca6
lndev2 <133123157+lndev2@users.noreply.github.com>
2023-11-19 23:40:44 +0100

    'all is working'

--- playing funs.js
+++ playing funs.js
@@ -6,7 +6,7 @@ let loop;
 let subdint;
 let sound = document.getElementById("myAudio");
 let pauset;
-
+let lenBar;
 let tick1 = 'tick.mp3'
 
 
@@ -20,7 +20,7 @@ function playAudio() {
 
 function toNext() {
 
-    ind = (ind + 1) % bar.length  // 4%4 = 0 
+    ind = (ind + 1) % lenBar  // 4%4 = 0 
     current = bar[ind]
 
     console.log('Next')
@@ -64,6 +64,7 @@ function start() {
     setSpeed()
     calculateInterval()
     setSubd()
+    lenBar = bar.length
     playLoop()
 }
 
@@ -82,42 +83,3 @@ function setSubd (){
 
 
 
-/* function playprova(){
-
-    
-    sound.play()
-    setTimeout(function () { sound.pause() }, 120)
-    
-
-}
-
-
-setInterval(function () { playprova() }, 125)
-
- */
-
-
-
-
-
-/* // Create a new Date object
-var currentDate = new Date();
-
-// Get the current time
-var currentTime = currentDate.toLocaleTimeString();
-
-// Display the current time
-console.log("Current time: " + currentTime);
- */
-
-
-
-
-/* let t1 = new Date();
-toNext()
-let t2 = new Date();
-
-let diff = t2 -t1
-
-
-console.log(diff) */
